f1fd4fd344b9a16a51c99b1975ffe1bd00539882
AyalaAbramson <ayala9858@gmail.com>
2026-05-12 12:51:50 +0300

    debug mode final

--- run_experiment.py
+++ run_experiment.py
@@ -13,11 +13,11 @@ IMAGE_DATABASE_PATH = r"C:\Users\User\Desktop\דברים\תמונות"
 IMAGE_SIZE = (800, 800)
 DISPLAY_TIME_SEC = 5
 FIXATION_TIME_SEC = 1.0
-TRIAL_REPETITIONS = 3    # Number of times each generated shape repeats
+TRIAL_REPETITIONS = 1    # Number of times each generated shape repeats
 
 # Toggle this to True to see grids, centers, and vertices.
 # Toggle to False for the clean, real experiment.
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 random.seed(42)
 
@@ -34,28 +34,29 @@ if not image_files:
 # =====================================================================
 # 3. PREPARE AUTOMATED EXPERIMENT TRIALS
 # =====================================================================
-auto_polygon_types = [4, 5]
-stretch_val = 50
+auto_polygon_types = [6]
+stretch_val = 20
 stretch_steps = [step * stretch_val for step in range(-2, 3)]
 rotation_options = [0, 90]
 fill_options = [True]
-concave_options = [None, 2]
+concave_options = [2]
+concave_ratio = [0.2]
 
 # Multiply by TRIAL_REPETITIONS
-base_auto_combos = list(itertools.product(auto_polygon_types, stretch_steps, rotation_options, fill_options, concave_options))
+base_auto_combos = list(itertools.product(auto_polygon_types, stretch_steps, rotation_options, fill_options, concave_options, concave_ratio))
 auto_combos = base_auto_combos * TRIAL_REPETITIONS
 random.shuffle(auto_combos)
 
 trial_data_auto = []
-for sides, s_amt, rot, is_filled, c_idx in auto_combos:
+for sides, s_amt, rot, is_filled, c_idx, c_ratio in auto_combos:
     tex = random.choice(image_files) if (image_files and is_filled) else None
 
     img, actual_points, base_points = generate_auto_polygon(
         num_vertices=sides, stretch_amt=s_amt, rotation_deg=rot,
-        target_idx=0, concave_idx=c_idx, texture_path=tex, size=IMAGE_SIZE
+        target_idx=0, concave_idx=c_idx, concave_ratio=c_ratio, texture_path=tex, size=IMAGE_SIZE
     )
     trial_data_auto.append({
-        "image": img, "type": "auto", "points": actual_points, "base_points": base_points
+        "image": img, "type": "auto", "points": actual_points, "base_points": base_points, "concave_idx": c_idx
     })
 
 # =====================================================================
@@ -80,7 +81,7 @@ for (radii, angles), rot, is_filled in manual_combos:
         texture_path=tex, size=IMAGE_SIZE
     )
     trial_data_manual.append({
-        "image": img, "type": "manual", "points": actual_points, "base_points": base_points
+        "image": img, "type": "manual", "points": actual_points, "base_points": base_points, "concave_idx": None
     })
 
 # =====================================================================
